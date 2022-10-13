959249618642307981ab5841dd3b8cd572123b80
lotar69 <lotardy@gmail.com>
2022-10-13 17:04:08 +0200

    backend done

--- django_todo_list/tasks/urls.py
+++ django_todo_list/tasks/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import index, add_collection, add_task, get_tasks
+from .views import index, add_collection, add_task, get_tasks, delete_task, delete_collection
+
 
 urlpatterns = [
     path('', index, name="home"),
     path('add-collection/', add_collection, name="add-collection"),
     path('add-task/', add_task, name="add-task"),
+    path('delete-task/<int:task_pk>/', delete_task, name="delete-task"),
+    path('delete-collection/<int:collection_pk>/', delete_collection, name="delete-collection"),
     path('get-tasks/<int:collection_pk>/', get_tasks, name="get-tasks"),
 ]
--- django_todo_list/tasks/views.py
+++ django_todo_list/tasks/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from django.template.loader import render_to_string
+from django.urls import reverse
 from django.utils.html import escape
 from django.utils.text import slugify
 
@@ -13,11 +14,14 @@ def index(request):
     context = {}
 
     collection_slug = request.GET.get("collection")
-    collection = Collection.get_default_collection()
-    if collection_slug:
-        collection = get_object_or_404(Collection, slug=collection_slug)
+    if not collection_slug:
+        Collection.get_default_collection()
+        return redirect(f"{reverse('home')}?collection=_defaut")
+
+    collection = get_object_or_404(Collection, slug=collection_slug)
 
     context["collections"] = Collection.objects.order_by("slug")
+    context["collection"] = collection
     context["tasks"] = collection.task_set.order_by('description')
 
     return render(request, "tasks/index.html", context=context)
@@ -29,18 +33,36 @@ def add_collection(request):
     if not created:
         return HttpResponse("La collection existe déja.", status=409)
 
-    return HttpResponse(f"<h2>{collection_name}</h2>")
+    return render(request, "tasks/collection.html", context={"collection": collection})
 
 
 def add_task(request):
     collection = Collection.objects.get(slug=request.POST.get("collection"))
     description = escape(request.POST.get("task-description"))
-    Task.objects.create(description=description, collection=collection)
-    return HttpResponse(description)
+    task = Task.objects.create(description=description, collection=collection)
+
+    return render(request, "tasks/task.html", context={"task": task})
+
+
+def delete_task(request, task_pk):
+    task = get_object_or_404(Task, pk=task_pk)
+    task.delete()
+
+    return HttpResponse("")
+
+
+def delete_collection(request, collection_pk):
+    collection = get_object_or_404(Collection, pk=collection_pk)
+    collection.delete()
+
+    return redirect("home")
 
 
 def get_tasks(request, collection_pk):
     collection = get_object_or_404(Collection, pk=collection_pk)
-    tasks= collection.task_set.order_by("description")
+    tasks = collection.task_set.order_by("description")
 
-    return render(request, "tasks/tasks.html", context={'tasks': tasks})
+    return render(request, "tasks/tasks.html", context={'tasks': tasks, 'collection': collection})
+
+
+
